ab4337203953d4db5d1ea45c740b17d211c11e9b
gabg4224 <gabg4224@gmail.com>
2022-04-18 14:30:38 -0300

    #3 commit

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ HELADERIA, REALIZAR UNA TIENDA DE HELADOS, QUE TENGAS, TE MUESTRE QUE HELADOS PU
  let salirTienda = document.querySelector('#salirTienda')
  let msjBienvenida = document.querySelector('#bienvenida')
  let msjCuerpo = document.querySelector('#cuerpo')
-
+ let montoContainer = document.querySelector('#montoContainer')
+ let botonPagar = document.querySelector('#botonPagar')
 
 
  let helados = [
@@ -30,7 +31,6 @@ HELADERIA, REALIZAR UNA TIENDA DE HELADOS, QUE TENGAS, TE MUESTRE QUE HELADOS PU
 
 
 let iniciarCompra = ()=>{
-    
 let nombre = prompt(`buen dia como te llamas?`)
 let monto = prompt(`cuanto dinero traes?`)
 let heladoBarato =helados.map((e)=>{return e[1]})
@@ -43,10 +43,14 @@ if(Math.min(...heladoBarato)> monto){
     mostrarHelados(nombre,monto)
 }
 
+botonPagar.addEventListener('click',()=>{
+    pagar(nombre,monto)
+})
 
 }
 
 let mostrarHelados= (nombre,monto)=>{
+
 msjBienvenida.innerHTML = `Buenos dias ${nombre}, por $${monto} te puedo ofrecer los siguientes helados:`
 
 let contenedorHelados = helados.filter((e)=>{
@@ -54,30 +58,56 @@ return e[1] <= monto})
 
 
 for (let i = 0; i < contenedorHelados.length; i++) {
-let escrito = `<div>${contenedorHelados[i][0]} por $${contenedorHelados[i][1]}</div>`
+let escrito = `<div class='helados'> ${i+1}. ${contenedorHelados[i][0]} por $${contenedorHelados[i][1]}</div>`
 msjCuerpo.innerHTML+= escrito
 }
 
 
+msjCuerpo.addEventListener('click',(e)=>{
+    e.preventDefault()
+let escrito = e.target.innerText
+
+let numero=  Number(escrito.slice(escrito.indexOf('$')+1))
+
+let valor = Number(montoContainer.innerHTML) + numero
+
+montoContainer.innerHTML = valor
+
+})
 
 }
 
 
+let pagar = (nombre, monto)=>{
+let cuenta = Number(montoContainer.innerHTML)
+if(cuenta == 0){
+alert(`no has llevado nada ${nombre} `)
+}else if(monto < cuenta){
+    alert(`lo siento ${nombre} no tienes suficiente dinero vuelve luego`)
+    window.location.reload()
+}else if(monto>cuenta){
+    alert(`muchas gracias ${nombre} tu vuelto es de ${monto-cuenta}`)
+    window.location.reload()
+}
 
+}
 
 
 
 entrarTienda.addEventListener('click',()=>{
 entrarTienda.classList.toggle('oculto')
 comprarConteiner.classList.toggle('oculto')
+montoContainer.classList.toggle('oculto')
+botonPagar.classList.toggle('oculto')
 })
 
 
 comprar.addEventListener('click',()=>{
 msjBienvenida.innerHTML=''
 msjCuerpo.innerHTML=''
-iniciarCompra()
 comprar.innerHTML= `otra compra`
+montoContainer.innerHTML = Number(0)
+iniciarCompra()
     })
 
     salirTienda.addEventListener('click',()=>{
